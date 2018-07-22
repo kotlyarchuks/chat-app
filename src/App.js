ef3f564a30716c135cf0c01faac47a4062824fda
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-07-22 14:12:13 +0200

    Chatkit integration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
-import AddMessageForm from './components/AddMessageForm';
+import React, { Component } from "react";
+import AddMessageForm from "./components/AddMessageForm";
 import MessageList from "./components/MessageList";
 import AddRoomForm from "./components/AddRoomForm";
 import RoomsList from "./components/RoomsList";
 
+import Chatkit from "@pusher/chatkit";
+import { instanceLocator, url, userId } from "./config";
+
 class App extends Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator,
+      userId,
+      tokenProvider: new Chatkit.TokenProvider({
+        url
+      })
+    });
+
+    chatManager.connect().then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: 12203449,
+        hooks: {
+          onNewMessage: message => {
+            console.log(`Message: ${message.text}`);
+          }
+        }
+      });
+    });
+  }
+
   render() {
     return (
       <div className="container">
---
+++ src/config.js
@@ -0,0 +1,8 @@
+const instanceLocator = "v1:us1:0ae249a2-be8d-4736-a9db-59b6a9c105bc";
+
+const url =
+  "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0ae249a2-be8d-4736-a9db-59b6a9c105bc/token";
+
+const userId = "kotlyarchuks";
+
+export { instanceLocator, url, userId };

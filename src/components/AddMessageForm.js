2921ef88902d3b9cd1908ad941e288b9512a143d
Denis Kotlyarchuk <kotlyarchukd@gmail.com>
2018-07-23 21:33:28 +0200

    Changed AddMessageForm - now it contains form

--- src/components/AddMessageForm.js
+++ src/components/AddMessageForm.js
@@ -3,16 +3,20 @@ import React, { Component } from "react";
 class AddMessageForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {};
   }
-  render() { 
-    return <div className="add-message-form grid__elem">
-        <input className="msg-input" placeholder="Your message" />
-        <button className="msg-button" type="submit">
-          <i className="fab fa-telegram-plane fa-3x icon" />
-        </button>
-      </div>;
+  render() {
+    return (
+      <div className="add-message-form grid__elem">
+        <form className="msg-form">
+          <input className="msg-input" placeholder="Your message" />
+          <button className="msg-button" type="submit">
+            <i className="fab fa-telegram-plane fa-3x icon" />
+          </button>
+        </form>
+      </div>
+    );
   }
 }
- 
+
 export default AddMessageForm;
